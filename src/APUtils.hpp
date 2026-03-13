900a7d2456b36901a61a6b2a6f8defd669c91ecc
matorblal-byte <matorblal@gmail.com>
2026-03-12 23:11:13 -0400

    .

--- src/APUtils.hpp
+++ src/APUtils.hpp
@@ -47,7 +47,7 @@ namespace APUtils {
 
     bool checkPortal(int id);
 
-    static void getStartingLevels(std::vector<int64_t> ids);
+    void getStartingLevels(std::vector<int64_t> ids);
 
     void startArchipelago(const char* url, const char* slot, const char* pass);
 }
