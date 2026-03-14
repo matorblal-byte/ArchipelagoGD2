91de74ed21754175812cfaa4c4388e954417d5ee
matorblal-byte <matorblal@gmail.com>
2026-03-14 00:00:36 -0400

    fix

--- src/PlayLayerHook.cpp
+++ src/PlayLayerHook.cpp
@@ -5,7 +5,7 @@
 int gdBaseID = 130820130;
 
 class $modify(APPlayLayer, PlayLayer) {
-    void void levelComplete() {
+    void levelComplete() {
         auto levelID = this->m_level->m_levelID.value() - 1; // -1 because of 0 indexing
         if (levelID > 100) {
             return; // reaches into thje zone of normal levels
@@ -15,4 +15,4 @@ class $modify(APPlayLayer, PlayLayer) {
         geode::log::info("completed level {}, sending itemid {}", level, levelID + gdBaseID);
         PlayLayer::levelComplete();
     }
-}
+};
