af058a726aabcd6dec0f9dfae5190bb6a28da3b7
matorblal-byte <matorblal@gmail.com>
2026-03-16 16:40:49 -0400

    literally commented every aastep of the way

--- src/PlayLayerHook.cpp
+++ src/PlayLayerHook.cpp
@@ -7,21 +7,35 @@
 
 class $modify(APPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        geode::log::info("init");
         if (!init(level, useReplay, dontCreateObjects)) {
             return false;
         }
+         geode::log::info("passed if");
         auto levelID = level->m_levelID.value();
+         geode::log::info("got level id");
         levelID = APUtils::checkIfTower(levelID);
+         geode::log::info("checked if it was tower lvl");
         if (levelID < 50) {
+             geode::log::info("was less than 50");
         auto ccsched = cocos2d::CCScheduler::get();
+         geode::log::info("obtained ccscheduler");
     if (!(APUtils::speed == 100)) {
+         geode::log::info("it was not equal to 1`00");
         double calctw = APUtils::speed/100;
+         geode::log::info("calculated the speed multi");
         auto difficulty = level->m_difficulty;
+         geode::log::info("got level diff");
         auto diffNum = static_cast<int>(difficulty);
+         geode::log::info("casted gddifficulty to int");
         double randWeight = geode::utils::random::generate(0.03, 0.05);
+         geode::log::info("randomweiught");
         double weightedcalctw = calctw * (1.1 - (randWeight * diffNum));
+         geode::log::info("got the weighted warp");
         double fullycalcedtw = std::clamp(weightedcalctw, 0.75, 2.50);
-        //ccsched->setTimeScale(fullycalcedtw);
+         geode::log::info("full");
+        ccsched->setTimeScale(fullycalcedtw);
+         geode::log::info("sped up");
     }
 }
         return true;
