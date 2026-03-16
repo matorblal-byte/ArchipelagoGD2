758c07b115961279e813092ecdb6e90a20330243
matorblal-byte <matorblal@gmail.com>
2026-03-16 17:43:06 -0400

    use mwethod update

--- src/PlayLayerHook.cpp
+++ src/PlayLayerHook.cpp
@@ -8,31 +8,19 @@
 class $modify(APPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
-         geode::log::info("passed if");
         auto levelID = level->m_levelID.value();
-         geode::log::info("got level id");
         levelID = APUtils::checkIfTower(levelID);
-         geode::log::info("checked if it was tower lvl");
         if (levelID < 50) {
-             geode::log::info("was less than 50");
         auto ccsched = cocos2d::CCScheduler::get();
-         geode::log::info("obtained ccscheduler");
     if (!(APUtils::speed == 100)) {
-         geode::log::info("it was not equal to 1`00");
         double calctw = APUtils::speed/100;
-         geode::log::info("calculated the speed multi");
         auto difficulty = level->m_difficulty;
-         geode::log::info("got level diff");
         auto diffNum = static_cast<int>(difficulty);
-         geode::log::info("casted gddifficulty to int");
-        double randWeight = geode::utils::random::generate(0.03, 0.05);
-         geode::log::info("randomweiught");
+        double randWeight = geode::utils::random::generate(0.01, 0.03);
         double weightedcalctw = calctw * (1.1 - (randWeight * diffNum));
-         geode::log::info("got the weighted warp");
         double fullycalcedtw = std::clamp(weightedcalctw, 0.75, 2.50);
-         geode::log::info("full");
-        ccsched->setTimeScale(fullycalcedtw);
-         geode::log::info("sped up");
+        geode::log::info("speed set: {}", fullycalcedtw);
+        ccsched->update(fullycalcedtw);
     }
 }
         return true;
@@ -45,7 +33,7 @@ class $modify(APPlayLayer, PlayLayer) {
         geode::log::info("level id is {}", levelID);
         if (!(ccsched->getTimeScale() == 1.00)) {
             this->m_isTestMode = true;
-            ccsched->setTimeScale(1.00);
+            ccsched->update(1.00);
         }
         if (levelID > 100 || this->m_isPracticeMode) {
             PlayLayer::levelComplete();
