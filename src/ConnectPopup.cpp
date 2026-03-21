bb417c605489f9db165570d63fb66d8558d1a9cd
matorblal-byte <matorblal@gmail.com>
2026-03-21 18:24:36 -0400

    This is so stuypids why is the file disappearimngh

--- src/ConnectPopup.cpp
+++ src/ConnectPopup.cpp
@@ -147,7 +147,7 @@ void onClick(CCObject* sender) {
                     log::warn("Unable to rename save. Error: {} Code: {}", error.message(), error.value());
                     return;
                 }
-                geode::utils::game::restart(false);
+                geode::utils::game::exit(false);
                 /*
                 APUtils::startArchipelago(url.c_str(), slot.c_str(), pass.c_str());
                 log::info("Connected to AP with url {}, slot {}, pass {}", url, slot, pass);
