845c9d123ff74dd3d17998022faeb2203afe1629
matorblal-byte <matorblal@gmail.com>
2026-03-29 20:31:46 -0400

    just make thewm go back to the menu for this

--- src/LevelSelectLayerHook.cpp
+++ src/LevelSelectLayerHook.cpp
@@ -3,7 +3,23 @@
 
 using namespace geode::prelude;
 
-class $modify(APLevelPage, LevelPage) {   
+class $modify(APLevelPage, LevelPage) {  
+    
+bool init(GJGameLevel* level) {
+    if (!LevelPage::init(level)) {
+        return false;
+    }
+    auto stars = GameStatsManager::sharedState()->getStat("6");
+    geode::log::info("number of stars: {}", stars);
+    if (stars > 180 && !goaled) {
+        auto goaled = true;
+        APUtils::goal();
+        AchievementNotifier::sharedState()->notifyAchievement("Win", "You have released all your locations! Good win", "APLogo.png"_spr, true);
+        FLAlertLayer::create("Warning", "Because you have goaled, you are unable to gain rewards from levels. Exit Archipelago mode to gain rewards again.", "Ok")->show();
+        PlayLayer::get()->m_isTestMode = true;
+    }
+    return true;
+}
     //thank you uproxide
     static void onModify(auto& self) {
         if (!self.setHookPriority("LevelPage::updateDynamicPage", -1)) {
