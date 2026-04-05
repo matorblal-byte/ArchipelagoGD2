202dc8c2348190ecb31359c279b29079b586db8a
matorblal-byte <matorblal@gmail.com>
2026-04-05 00:41:15 -0400

    koops

--- src/GJGameBaseLayerHook.cpp
+++ src/GJGameBaseLayerHook.cpp
@@ -11,13 +11,12 @@ class $modify(APGJBaseGameLayer, GJBaseGameLayer) {
         if (APUtils::coinsEnabled) {
             auto levelID = levelObj->m_levelID.value();
             levelID = APUtils::checkIfTower(levelID);
-            levelID -= 1;
             if (levelID < 100) {
                 for (int i = 0; i < 3; i++) {
                     auto key = levelObj->getCoinKey(i+1);
                     auto hasCoin = GameStatsManager::sharedState()->hasSecretCoin(key);
                     if (hasCoin) {
-                        auto itemID = (levelID * 1000) + (i+1) + 130820130 + 1;
+                        auto itemID = (levelID * 1000) + (i+1) + 130820130;
                         APUtils::sendItem(itemID);
                         geode::log::info("sent coin item with id {}", itemID);
                     } else {
